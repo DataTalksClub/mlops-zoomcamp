a19655c75e34837598a61403573c466c081aa0d1
Alexey Grigorev <alexeygrigorev@users.noreply.github.com>
2022-05-25 09:22:06 +0200

    Homework 2 (#52)
    * adding the files for the 2nd homework
    
    * Update homework.md
    
    * Update homework.md
    
    * Update train.py
    
    * Update hpo.py
    
    * Update homework.md
    
    * Update README.md
    
    Co-authored-by: Cristian Martinez <martinezcristianjavier@gmail.com>

--- 02-experiment-tracking/README.md
+++ 02-experiment-tracking/README.md
@@ -1,11 +1,5 @@
 # 2. Experiment tracking and model management
 
-Note: in the videos, Cristian uses Jupyter in VS code and runs everything locally
-
-But if you set up a VM in the previous module, you can keep using it
-and use the usual Jupyter from your browser. There's no significant
-difference between using Jupyter with VS code and without
-
 
 ## 2.1 Experiment tracking intro
 
@@ -21,6 +15,11 @@ difference between using Jupyter with VS code and without
   <img src="images/thumbnail-2-02.jpg">
 </a>
 
+Note: in the videos, Cristian uses Jupyter in VS code and runs everything locally
+
+But if you set up a VM in the previous module, you can keep using it
+and use the usual Jupyter from your browser. There's no significant
+difference between using Jupyter with VS code and without
 
 
 ## 2.3 Experiment tracking with MLflow
@@ -55,7 +54,7 @@ COMING SOON
 
 ## 2.7 Homework
 
-
+More information here: [homework.md](homework.md)
 
 
 ## Notes
---
+++ 02-experiment-tracking/homework/hpo.py
@@ -0,0 +1,69 @@
+import argparse
+import os
+import pickle
+
+import mlflow
+import numpy as np
+from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
+from hyperopt.pyll import scope
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("random-forest-hyperopt")
+
+
+def load_pickle(filename):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+def run(data_path, num_trials):
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+
+    def objective(params):
+
+        rf = RandomForestRegressor(**params)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+
+        return {'loss': rmse, 'status': STATUS_OK}
+
+    search_space = {
+        'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
+        'n_estimators': scope.int(hp.quniform('n_estimators', 10, 50, 1)),
+        'min_samples_split': scope.int(hp.quniform('min_samples_split', 2, 10, 1)),
+        'min_samples_leaf': scope.int(hp.quniform('min_samples_leaf', 1, 4, 1)),
+        'random_state': 42
+    }
+
+    rstate = np.random.default_rng(42)  # for reproducible results
+    fmin(
+        fn=objective,
+        space=search_space,
+        algo=tpe.suggest,
+        max_evals=num_trials,
+        trials=Trials(),
+        rstate=rstate
+    )
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    parser.add_argument(
+        "--max_evals",
+        default=50,
+        help="the number of parameter evaluations for the optimizer to explore."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path, args.max_evals)
