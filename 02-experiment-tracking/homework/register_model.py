988fd48c954168e75cab6efabfd2ecfc34e19a27
Ubuntu <ubuntu@ip-172-31-46-175.ec2.internal>
2022-05-28 14:49:10 +0000

    Finished week 2 homework

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -3,11 +3,8 @@ import os
 import pickle
 
 import mlflow
-from hyperopt import hp, space_eval
-from hyperopt.pyll import scope
 from mlflow.entities import ViewType
 from mlflow.tracking import MlflowClient
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
 HPO_EXPERIMENT_NAME = "random-forest-hyperopt"
@@ -17,34 +14,16 @@ mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
 
-SPACE = {
-    'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
-    'n_estimators': scope.int(hp.quniform('n_estimators', 10, 50, 1)),
-    'min_samples_split': scope.int(hp.quniform('min_samples_split', 2, 10, 1)),
-    'min_samples_leaf': scope.int(hp.quniform('min_samples_leaf', 1, 4, 1)),
-    'random_state': 42
-}
-
-
 def load_pickle(filename):
     with open(filename, "rb") as f_in:
         return pickle.load(f_in)
 
 
-def train_and_log_model(data_path, params):
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+def inference(data_path, model):
     X_test, y_test = load_pickle(os.path.join(data_path, "test.pkl"))
 
     with mlflow.start_run():
-        params = space_eval(SPACE, params)
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-
-        # evaluate model on the validation and test sets
-        valid_rmse = mean_squared_error(y_valid, rf.predict(X_valid), squared=False)
-        mlflow.log_metric("valid_rmse", valid_rmse)
-        test_rmse = mean_squared_error(y_test, rf.predict(X_test), squared=False)
+        test_rmse = mean_squared_error(y_test, model.predict(X_test), squared=False)
         mlflow.log_metric("test_rmse", test_rmse)
 
 
@@ -60,15 +39,30 @@ def run(data_path, log_top):
         max_results=log_top,
         order_by=["metrics.rmse ASC"]
     )
-    for run in runs:
-        train_and_log_model(data_path=data_path, params=run.data.params)
 
+    # Getting run ids for all top 5 models
+    run_ids = [dict(dict(run)['info'])['run_id'] for run in runs]
+    
+    for id in run_ids:
+        model_uri = f"runs:/{id}/model"
+        model = mlflow.pyfunc.load_model(model_uri)
+        inference(data_path=data_path, model=model)
+        
     # select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(experiment_ids=experiment.experiment_id,
+                                  run_view_type=ViewType.ACTIVE_ONLY,
+                                  max_results=1,
+                                  order_by=["metrics.test_rmse ASC"]
+                                  )
 
+    # Getting the best run id for test dataset
+    best_run_id = [dict(dict(run)['info'])['run_id'] for run in best_run][0]
+    
     # register the best model
-    # mlflow.register_model( ... )
+    model_name = "green_taxi_duration_model"
+    model_uri = f"runs:/{best_run_id}/model"
+    mlflow.register_model(model_uri=model_uri, name=model_name)
 
 
 if __name__ == '__main__':
--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import pickle
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
@@ -16,11 +17,17 @@ def run(data_path):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("nyc-taxi-experiment-rf")
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+    with mlflow.start_run():
+
+        mlflow.sklearn.autolog()
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
