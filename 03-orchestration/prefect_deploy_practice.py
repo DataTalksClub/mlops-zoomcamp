71c05d4a224fc1358a1783e72fa66ad934729cfa
Ubuntu <ubuntu@ip-172-31-46-175.ec2.internal>
2022-06-05 08:13:36 +0000

    Practiced to spinup local deployment with prefect

---
+++ 03-orchestration/prefect_deploy_practice.py
@@ -0,0 +1,159 @@
+import pickle
+from datetime import timedelta
+
+import mlflow
+import pandas as pd
+import xgboost as xgb
+from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
+from hyperopt.pyll import scope
+from prefect import flow, task
+from prefect.deployments import DeploymentSpec
+from prefect.flow_runners import SubprocessFlowRunner
+from prefect.orion.schemas.schedules import IntervalSchedule
+from prefect.task_runners import SequentialTaskRunner
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import mean_squared_error
+
+
+@task(retries=2)
+def read_dataframe(filename):
+    df = pd.read_parquet(filename)
+
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+
+    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    
+    return df
+
+def xgboost_matrix(X, y):
+    combined = xgb.DMatrix(X, label=y)
+    return combined
+
+
+@task(retries=2)
+def add_features(df_train, df_val):
+    # df_train = read_dataframe(train_path)
+    # df_val = read_dataframe(val_path)
+
+    print(len(df_train))
+    print(len(df_val))
+
+    df_train['PU_DO'] = df_train['PULocationID'] + '_' + df_train['DOLocationID']
+    df_val['PU_DO'] = df_val['PULocationID'] + '_' + df_val['DOLocationID']
+
+    categorical = ['PU_DO'] #'PULocationID', 'DOLocationID']
+    numerical = ['trip_distance']
+
+    dv = DictVectorizer()
+
+    train_dicts = df_train[categorical + numerical].to_dict(orient='records')
+    X_train = dv.fit_transform(train_dicts)
+
+    val_dicts = df_val[categorical + numerical].to_dict(orient='records')
+    X_val = dv.transform(val_dicts)
+
+    target = 'duration'
+    y_train = df_train[target].values
+    y_val = df_val[target].values
+
+    return X_train, X_val, y_train, y_val, dv
+
+@task(retries=2)
+def train_model_search(X_train, X_val, y_train, y_val):
+
+    train = xgboost_matrix(X_train, y_train)
+    valid = xgboost_matrix(X_val, y_val) 
+    def objective(params):
+        with mlflow.start_run():
+            mlflow.set_tag("model", "xgboost")
+            mlflow.log_params(params)
+            booster = xgb.train(
+                params=params,
+                dtrain=train,
+                num_boost_round=100,
+                evals=[(valid, 'validation')],
+                early_stopping_rounds=50
+            )
+            y_pred = booster.predict(valid)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
+
+        return {'loss': rmse, 'status': STATUS_OK}
+
+    search_space = {
+        'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
+        'learning_rate': hp.loguniform('learning_rate', -3, 0),
+        'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
+        'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
+        'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+        'objective': 'reg:squarederror',
+        'seed': 42
+    }
+
+    best_result = fmin(
+        fn=objective,
+        space=search_space,
+        algo=tpe.suggest,
+        max_evals=1,
+        trials=Trials()
+    )
+    return best_result
+
+@task(retries=2)
+def train_best_model(X_train, X_val, y_train, y_val, dv, params):
+
+    train = xgboost_matrix(X_train, y_train)
+    valid = xgboost_matrix(X_val, y_val) 
+
+    with mlflow.start_run():
+        
+        best_params = params
+        best_params['max_depth'] = int(best_params['max_depth'])
+        best_params['objective'] = 'reg:squarederror'
+        best_params['seed'] = 42
+
+        mlflow.log_params(best_params)
+
+        booster = xgb.train(
+            params=best_params,
+            dtrain=train,
+            num_boost_round=100,
+            evals=[(valid, 'validation')],
+            early_stopping_rounds=50
+        )
+
+        y_pred = booster.predict(valid)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
+
+        with open("models/preprocessor.b", "wb") as f_out:
+            pickle.dump(dv, f_out)
+        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+
+        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+
+@flow(task_runner=SequentialTaskRunner())
+def main(train_path: str="/home/ubuntu/data/green_tripdata_2021-01.parquet",
+        val_path: str="/home/ubuntu/data/green_tripdata_2021-02.parquet"):
+    
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("nyc-taxi-experiment")
+    X_train = read_dataframe(train_path)
+    X_val = read_dataframe(val_path)
+    X_train, X_val, y_train, y_val, dv = add_features(X_train, X_val).result()
+    best_params = train_model_search(X_train, X_val, y_train, y_val).result()
+    train_best_model(X_train, X_val, y_train, y_val, dv, best_params)
+
+
+DeploymentSpec(flow=main,
+               schedule=IntervalSchedule(interval=timedelta(minutes=5)),
+               name="model-training",
+               flow_runner=SubprocessFlowRunner(),
+               tags=["ml"])
