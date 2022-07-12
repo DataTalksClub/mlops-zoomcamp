91cfc49c7bc741488d1521ec2e0b4182b314a357
Emeli Dral <emeli.dral@gmail.com>
2022-07-12 22:54:45 +0200

    Added target loading to a database

--- 05-monitoring/evidently_service/app.py
+++ 05-monitoring/evidently_service/app.py
@@ -199,6 +199,9 @@ def configure_service():
         reference_file = dataset_options['reference_file']
         logging.info(f"Load reference data for dataset {dataset_name} from {reference_file}")
         reference_data = pq.read_table(reference_file).to_pandas()
+        reference_data['duration'] = reference_data.lpep_dropoff_datetime - reference_data.lpep_pickup_datetime
+        reference_data.duration = reference_data.duration.apply(lambda td: td.total_seconds() / 60)
+        reference_data = reference_data[(reference_data.duration >= 1) & (reference_data.duration <= 60)]
         datasets[dataset_name] = LoadedDataset(
             name=dataset_name,
             references=reference_data,
