17c07104d57fff46ff0709ab892dc6e2d647904d
Sean Hickey <seanhickey@live.ie>
2023-06-18 18:57:27 +0000

    adding optuna and mlflow pages

--- mlops_jupyter_book/utils/utils.py
+++ mlops_jupyter_book/utils/utils.py
@@ -6,8 +6,11 @@ import itables.options as opt
 import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.io as pio
+from enum import Enum
 
 ROOT_DIR = Path(__file__).resolve().parents[2]
+AWS_ACCESS_KEY = "AKIASK6MAGUCNG2GWRSX"
+AWS_SCRET_KEY = "neomBVqvDRnF9YKjFDpy4ySBxbTDmxy1HCgSEQ8J"
 
 
 def init_jb_table_style():
@@ -26,3 +29,31 @@ def init_jb_table_style():
 
 def render_itable(df: pd.DataFrame, caption: str | None = None, autowidth: bool = False, **kwargs) -> Any:
     itables.show(df, caption=caption, autoWidth=autowidth, **kwargs)
+
+
+class FileTypes(Enum):
+    PARQUET = {"type": "parquet", "function": pd.read_parquet}
+    CSV = {"type": "csv", "function": pd.read_csv}
+
+
+def _import_data(data_path: Path) -> pd.DataFrame:
+    """Function that will check import the data. This function will check the type of the file and if it is not in a prescribed list of
+    files (See FileTypes class) then an exception will be thrown.
+
+    Args:
+        data_path (Path): Path to the data to be imported
+
+    Raises:
+        Exception: Exception if the filetype to be imported is now a valid FileTypes class value
+
+    Returns:
+        pd.DataFrame: pandas DataFrame containing the data that was imported from the data path
+    """
+    file_type = str(data_path).split(".")[-1]
+    if file_type not in [e.value["type"] for e in FileTypes]:
+        raise Exception(
+            f"file type {file_type} is not currently supported, please use one of the following {[e.value for e in FileTypes]}"
+        )
+    else:
+        df = FileTypes[file_type.upper()].value["function"](data_path)
+        return df
