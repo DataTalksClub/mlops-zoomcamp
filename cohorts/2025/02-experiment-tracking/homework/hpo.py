19708b58d5655fa7f996a280c5c2a9b8db4eef8b
Pastor Soto <sotoblanco263542@gmail.com>
2025-05-24 16:19:35 -0400

    Update hpo.py to have the new version of the function rmse

--- cohorts/2025/02-experiment-tracking/homework/hpo.py
+++ cohorts/2025/02-experiment-tracking/homework/hpo.py
@@ -6,7 +6,7 @@ import numpy as np
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("random-forest-hyperopt")
@@ -38,7 +38,7 @@ def run_optimization(data_path: str, num_trials: int):
         rf = RandomForestRegressor(**params)
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = root_mean_squared_error(y_val, y_pred)
 
         return {'loss': rmse, 'status': STATUS_OK}
 
