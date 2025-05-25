86545156f2b8d8a9cc04cead19ac0b9e06c13748
Pastor Soto <sotoblanco263542@gmail.com>
2025-05-25 17:02:24 -0400

    Update register_model.py
    the new versions of sklearn does not support mean_squared_error with the agreement squared false, it was changed to root_mean_squared_error

--- cohorts/2025/02-experiment-tracking/homework/register_model.py
+++ cohorts/2025/02-experiment-tracking/homework/register_model.py
@@ -6,7 +6,7 @@ import mlflow
 from mlflow.entities import ViewType
 from mlflow.tracking import MlflowClient
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 HPO_EXPERIMENT_NAME = "random-forest-hyperopt"
 EXPERIMENT_NAME = "random-forest-best-models"
@@ -36,9 +36,9 @@ def train_and_log_model(data_path, params):
         rf.fit(X_train, y_train)
 
         # Evaluate model on the validation and test sets
-        val_rmse = mean_squared_error(y_val, rf.predict(X_val), squared=False)
+        val_rmse = root_mean_squared_error(y_val, rf.predict(X_val))
         mlflow.log_metric("val_rmse", val_rmse)
-        test_rmse = mean_squared_error(y_test, rf.predict(X_test), squared=False)
+        test_rmse = root_mean_squared_error(y_test, rf.predict(X_test))
         mlflow.log_metric("test_rmse", test_rmse)
 
 
